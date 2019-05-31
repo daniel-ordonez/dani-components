054ecec54caf2330294a0f9623ca38197da000a8
Daniel Ordonez <road_2015@hotmail.com>
2019-05-31 14:27:22 -0600

    InputTextBase component

---
+++ components/InputBase/InputTextBase.vue
@@ -0,0 +1,45 @@
+<script>
+const INPUT_STATE = {
+    'INITIAL': 0,
+    'ERROR': 2,
+    'SUCCESS': 1,
+}
+export default {
+    props: {
+        value: { type: String, default: '' },
+        label: { type: String, default: '' },
+        id: { type: String, default: '' },
+        required: { type: Boolean, default: false },
+        rules: { type: Array, default: () => [str => str.length] },
+        trim: { type: Boolean, default: false },
+        clear: { type: Boolean, default: false }
+    },
+    data: () => ({
+        focused: false,
+        content: '',
+        validator: null,
+        state: INPUT_STATE.INITIAL
+    }),
+    computed: {
+        prepend () { return this.$slots.prepend },
+        append () { return this.$slots.append },
+        inputId () { return this.id.length ? this.id : `input__${this._uid}` }
+    },
+    methods: {
+        focus (event) {
+            this.focused = true
+            this.$emit('focus', event)
+        },
+        blur (event) {
+            this.focused = false
+            this.$emit('blur', event)
+        },
+        reset () {
+            this.content = ''
+            this.$emit('input', '')
+            this.state = INPUT_STATE.INITIAL
+        }
+
+    }
+}
+</script>
---
+++ components/InputText/InputText.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="input-text">
+        <div v-if="label.length" class="input-text__label">
+            <label :for="inputId">{{label}}</label>
+        </div>
+        <div class="input-text__wrapper">
+            <div v-if="prepend" class="inpute-text__prepend">
+                <slot name="prepend"/>
+            </div>
+
+            <input 
+                :id="inputId"
+                v-model="inputValue"
+                v-bind="$attrs"
+                class="input-text__text"
+                type="text"
+                @focus="focus"
+                @blur="blur">
+            
+            <div v-if="append" class="inpute-text__append">
+                <slot name="append"/>
+            </div>
+            
+            <div v-if="clear" class="input-text__clear">
+                <button v-if="inputValue" class="btn--icon" @click="inputValue = null">
+                    x
+                </button>
+            </div>
+
+        </div>
+    </div>
+</template>
