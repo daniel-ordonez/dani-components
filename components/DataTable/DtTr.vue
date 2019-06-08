7f4c13a414b6dcb43f0fbda71d11720bd9de6c2a
Daniel Ordonez <road_2015@hotmail.com>
2019-06-07 19:40:46 -0600

    data table table row

---
+++ components/DataTable/DtTr.vue
@@ -0,0 +1,23 @@
+<template>
+    <tr>
+        <slot/>
+    </tr>
+</template>
+
+<script>
+export default {
+    mounted () {
+        let el = this.$el
+        el.addEventListener('click', () => {
+            this.$emit('click')
+        })
+        el.addEventListener('mouseover', () => {
+            this.$emit('mouseover')
+        })
+        el.addEventListener('mouseleave', () => {
+            this.$emit('mouseleave')
+        })
+    }
+}
+</script>
+
