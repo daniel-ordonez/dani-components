67fb2d78363a96740f00b88be3587a41861f38a3
Daniel Ordonez <road_2015@hotmail.com>
2019-06-29 22:34:07 -0600

    update signal names for modal

--- components/Modal/Modal.vue
+++ components/Modal/Modal.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="modal">
         <transition-group name="modal" 
-            @after-enter="$emit('afterEnter')"
-            @after-leave="$emit('afterleave')">
+            @after-enter="$emit('after-enter')"
+            @after-leave="$emit('after-leave')">
             <template v-if="value">
                 <div class="modal-screen" @click.stop="outside" key="modal-screen"/>
                 <div class="modal-container" key="modal-container" >
